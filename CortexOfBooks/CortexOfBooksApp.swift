70b2c6fd6d1b6a4ad16870800a8fcee79805680d
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-11-04 23:39:16 +0100

    Initial Commit

---
+++ CortexOfBooks/CortexOfBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  CortexOfBooksApp.swift
+//  CortexOfBooks
+//
+//  Created by Zaid Neurothrone on 2022-11-04.
+//
+
+import SwiftUI
+
+@main
+struct CortexOfBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
